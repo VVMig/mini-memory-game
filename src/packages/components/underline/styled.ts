5c9d0848a22786195c54b885de44de0279961e2b
Vova Migay <v.migay@itransition.com>
2021-06-10 20:26:44 +0300

    add packages

---
+++ src/packages/components/underline/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Underline = styled.div`
+  position: absolute;
+  width: 0;
+  height: 1px;
+  bottom: 0px;
+  transition: 0.1s linear;
+  cursor: pointer;
+`;
+
+export const Styled = {
+  Underline,
+};
