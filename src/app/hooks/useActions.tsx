1ad51f7c971a1e22f028919d577170560c25480e
Vladimir Migay <vova.migay123@gmail.com>
2021-06-14 02:26:07 +0300

    rename action

--- src/app/hooks/useActions.tsx
+++ src/app/hooks/useActions.tsx
@@ -1,10 +1,10 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import * as userActions from '../store/actionCreators';
+import * as actions from '../store/actionCreators';
 
 export const useActions = () => {
   const dispatch = useDispatch();
 
-  return bindActionCreators(userActions, dispatch);
+  return bindActionCreators(actions, dispatch);
 };
