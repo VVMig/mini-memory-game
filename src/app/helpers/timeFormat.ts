e781f7b51d949b284420d950862247061e5603a6
Vladimir Migay <vova.migay123@gmail.com>
2021-06-14 11:58:37 +0300

    add hours instead days

--- src/app/helpers/timeFormat.ts
+++ src/app/helpers/timeFormat.ts
@@ -1,4 +1,15 @@
 import moment from 'moment';
 
-export const timeFormat = (seconds: number) =>
-  moment().startOf('day').seconds(seconds).format('H:mm:ss');
+export const timeFormat = (seconds: number) => {
+  const secondsInDay = 86400;
+  const hoursInDay = 24;
+
+  const days = seconds / secondsInDay;
+  const hours = Math.floor(hoursInDay * days);
+
+  return moment()
+    .startOf('year')
+    .seconds(seconds)
+    .format('H:mm:ss')
+    .replace(/^\d{1,2}/, `${hours}`);
+};
